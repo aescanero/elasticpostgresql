e6de544c781eab959798ba40fa62e048206449d3
Alejandro Escanero Blanco <aescanero@gmail.com>
2017-08-28 15:13:32 +0200

    Adding SQL and config to support auto register containers in Zabbix

--- tests/docker_postgresql_bucardo_alpine/usr/local/sbin/precheck.sh
+++ tests/docker_postgresql_bucardo_alpine/usr/local/sbin/precheck.sh
@@ -8,9 +8,9 @@ then
   ln -s /data/vol/${HOSTNAME} /data/postgresql
   chown postgres /data/postgresql/
   mkdir -p /run/postgresql && chown postgres /run/postgresql
+  su postgres -c "/usr/bin/initdb --pgdata /data/postgresql/" 
   echo "listen_addresses ='*'" >>/data/postgresql/postgresql.conf
   echo "host    all             all             0/0            md5" >>/data/postgresql/pg_hba.conf
-  su postgres -c "/usr/bin/initdb --pgdata /data/postgresql/" 
 else
   ln -s /data/vol/${HOSTNAME} /data/postgresql
   chown postgres /data/postgresql/
@@ -19,13 +19,23 @@ fi
 
 supervisorctl start postgresql
 sleep 10
-if su - postgres -c "echo '\l'|psql"|grep zabbix >/dev/null
+if ! su - postgres -c "echo '\l'|psql"|grep zabbix >/dev/null
 then
   su - postgres -c "echo CREATE USER zabbix WITH PASSWORD \'test_zabbix\'\;|psql"
   su - postgres -c "echo CREATE DATABASE zabbix WITH OWNER \'zabbix\' ENCODING \'UTF8\'\;|psql"
   cat /data/build/zabbix/schema.sql |psql -q -U zabbix zabbix
   cat /data/build/zabbix/images.sql |psql -q -U zabbix zabbix
   cat /data/build/zabbix/data.sql |psql -q -U zabbix zabbix
+  su - postgres -c "echo INSERT INTO actions VALUES \(7, \'Autodiscover Containers\', 2, 0, 0, 0, \'Auto registration: {HOST.HOST}\', \'Host name: {HOST.HOST}\'\|\|chr\(10\)\|\|\'Host IP: {HOST.IP}\'\|\|chr\(10\)\|\|\'Agent port: {HOST.PORT}\', \'\', \'\', \'\', 1\)\; |psql -U zabbix zabbix"
+  su - postgres -c "echo INSERT INTO operations VALUES \(11, 7, 2, 0, 1, 1, 0, 0\)\; |psql -U zabbix zabbix"
+  su - postgres -c "echo INSERT INTO operations VALUES \(12, 7, 4, 0, 1, 1, 0, 0\)\; |psql -U zabbix zabbix"
+  su - postgres -c "echo INSERT INTO operations VALUES \(13, 7, 6, 0, 1, 1, 0, 0\)\; |psql -U zabbix zabbix"
+  su - postgres -c "echo INSERT INTO opgroup VALUES \(2, 12, 2\)\; |psql -U zabbix zabbix"
+  su - postgres -c "echo INSERT INTO optemplate VALUES \(2, 13, 10001\)\; |psql -U zabbix zabbix"
+
+#  su - postgres -c "echo INSERT INTO actions VALUES (7, \'Autodiscover Containers\', 2, 0, 0, 0, \'Auto registration: {HOST.HOST}\', \'Host name: {HOST.HOST}
+#  Host IP: {HOST.IP}
+#  Agent port: {HOST.PORT}\', \'\', \'\', \'\', 1)\; |psql"
 fi
 supervisorctl start postcheck.py
 
