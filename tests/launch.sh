0eaba66d0a8bc080fba0c1b1309c57945ddd5001
Alejandro Escanero <aescanero@gmail.com>
2017-09-13 08:39:05 +0200

    Adding mysql jdbc and postgresql jdbc packages to image

--- tests/launch.sh
+++ tests/launch.sh
@@ -3,15 +3,15 @@
 cd ~/tests/
 sudo docker build -t elasticpostgresql:psql_v1 -f docker_postgresql_bucardo_alpine/Dockerfile docker_postgresql_bucardo_alpine/.
 sudo docker build -t elasticpostgresql:zabbix_v1 -f docker_zabbix/Dockerfile docker_zabbix/.
-sudo docker build -t elasticpostgresql:docker_tomcat_alpine_v1 -f docker_tomcat_alpine/Dockerfile docker_tomcat_alpine/.
+sudo docker build -t elasticpostgresql:docker_magnolia_alpine_v1 -f docker_magnolia_alpine/Dockerfile docker_magnolia_alpine/.
 sudo docker network create -d overlay registrynetwork
 sudo docker service create --detach=true --endpoint-mode vip --network registrynetwork --publish 5000:5000 --restart-condition any --name registry registry:2
 sudo docker tag elasticpostgresql:psql_v1 localhost:5000/elasticpostgresql:psql_v1
 sudo docker push localhost:5000/elasticpostgresql:psql_v1
 sudo docker tag elasticpostgresql:zabbix_v1 localhost:5000/elasticpostgresql:zabbix_v1
 sudo docker push localhost:5000/elasticpostgresql:zabbix_v1
-sudo docker tag elasticpostgresql:docker_tomcat_alpine_v1 localhost:5000/elasticpostgresql:docker_tomcat_alpine_v1
-sudo docker push localhost:5000/elasticpostgresql:docker_tomcat_alpine_v1
+sudo docker tag elasticpostgresql:docker_magnolia_alpine_v1 localhost:5000/elasticpostgresql:docker_magnolia_alpine_v1
+sudo docker push localhost:5000/elasticpostgresql:docker_magnolia_alpine_v1
 
 sudo docker stack deploy demo -c docker-compose-test.yml
 sudo docker stack ps demo
